01d194419a1478fa2c6780b6bc87aca1eb9984fe
Maven-Nexus <167427355+Maven-Nexus@users.noreply.github.com>
2024-07-26 21:01:12 +0300

    Spark-Md 🤍✨

--- set.js
+++ set.js
@@ -9,7 +9,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || 'zokk',
     PREFIXE: process.env.PREFIX || "~",
-    OWNER_NAME: process.env.OWNER_NAME || "Spark-Md",
+    OWNER_NAME: process.env.OWNER_NAME || "Lil Benjamin",
     NUMERO_OWNER : process.env.NUMERO_OWNER || "254769702239",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'off',
